179bec0f40d8fabd2d17918c1871cd80cc00f8e3
kevin <kevinchannon@gmail.com>
2021-11-26 07:10:51 +0000

    Adding some tests for async things

--- play/play.cpp
+++ play/play.cpp
@@ -1,5 +1,9 @@
 #include "CppUnitTest.h"
 
+#include <future>
+#include <atomic>
+#include <chrono>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace play
@@ -10,6 +14,29 @@ namespace play
 		
 		TEST_METHOD(SimpleAsyncTest)
 		{
+			auto was_called = false;
+			auto f = std::async([&was_called]() {was_called = true; });
+			f.get();
+
+			Assert::IsTrue(was_called);
+		}
+
+		TEST_METHOD(AsyncWithAtomicStopFlag)
+		{
+			auto keep_running = std::atomic<bool>{ true };
+			auto was_called = false;
+
+			auto f = std::async([&was_called, &keep_running]() {
+				while (keep_running);
+				was_called = true;
+				});
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+			keep_running = false;
+
+			f.get();
+
+			Assert::IsTrue(was_called);
 		}
 	};
 }
