6ce0e52b4fc979282d9dea6f083f66b54051a6f6
kevin <kevinchannon@gmail.com>
2021-11-26 07:00:42 +0000

    Add project files.

---
+++ play/play.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace play
+{
+	TEST_CLASS(Threading)
+	{
+	public:
+		
+		TEST_METHOD(SimpleAsyncTest)
+		{
+		}
+	};
+}
